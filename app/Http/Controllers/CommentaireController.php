2ed8fe969bd2b2607f6a51dc5d10a4fe6959259a
loctetgenius <sagnamoussa546@gmail.com>
2024-06-03 16:49:09 +0000

    Ajout de la fonctionnalité afficher commentaire

--- app/Http/Controllers/CommentaireController.php
+++ app/Http/Controllers/CommentaireController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Propriete;
+use App\Models\Commentaire;
 use Illuminate\Http\Request;
 
 class CommentaireController extends Controller
 {
-    //
+    public function afficherCommentaire($propriete_id)
+    {
+        $propriete = Propriete::findOrFail($propriete_id);
+        $commentaires = $propriete->commentaires;
+
+        return view('commentaires.index', compact('propriete', 'commentaires'));
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentaireController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('commentaires', [CommentaireController::class, 'afficherCommentaire'])->name('proprietes.commentaires.afficher');
+Route::post('ajouterCommentaire/{id}/traitement', [CommentaireController::class, 'ajouterCommentaireTraitement'])->name('comment.store');
+
