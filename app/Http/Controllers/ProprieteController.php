970b6ab5ca06cfad2b5186d5c7e3beeb01a19967
celineMend <celine.mendy@unchk.edu.sn>
2024-06-04 11:00:10 +0100

    formulaire pour la  modification

--- app/Http/Controllers/ProprieteController.php
+++ app/Http/Controllers/ProprieteController.php
@@ -41,4 +41,23 @@ class ProprieteController extends Controller
 
         return view('proprietes.detail', compact('propriete', 'commentaires'));
     }
+
+    public function modifier_proprietes($id){
+        $proprietes = Propriete::find($id);
+        return view('proprietes.modifier', compact('proprietes'));
+    }
+
+    public function modifier_proprietes_traitement(Request $request)
+    {
+
+
+
+    $propriete = Propriete::find($request->id);
+    $propriete->nom = $request->nom;
+    $propriete->description = $request->description;
+    $propriete->image = $request->image;
+    $propriete->save();
+    return redirect('/proprietes')->with('status',' le propriete a été modifier avec succès');
+    }
+
 }
